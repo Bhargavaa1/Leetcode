0e0012b6ea8d0e080ea8b4e3ea5202cf24b3b64f
Bhargavaa1 <abhargava@g.hmc.edu>
2022-06-04 12:57:13 -0400

    Contains Duplicate

--- Array/containsDuplicate.py
+++ Array/containsDuplicate.py
@@ -1,10 +1,13 @@
+from typing import *
 # 217. Contains Duplicate
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-from typing import List
-def containsDuplicate(nums: List[int]) -> bool:
+def containsDuplicate1(nums: List[int]) -> bool:
     nums.sort()
     for i in range(len(nums)-1):
         if nums[i] == nums[i+1]:
             return True
     return False
+
+def containsDuplicate2(nums: List[int]) -> bool:
+    return len(nums) != len(set(nums))
     
