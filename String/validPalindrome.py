5f5c942e7ec910101a80425dbf8fed3bc9e2b142
Bhargavaa1 <abhargava@g.hmc.edu>
2022-07-09 18:43:55 -0400

    Valid Palindrome Two Pointer Solution

--- String/validPalindrome.py
+++ String/validPalindrome.py
@@ -2,15 +2,40 @@
 # A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
 # Alphanumeric characters include letters and numbers.
 # Given a string s, return true if it is a palindrome, or false otherwise.
-def isPalindrome(s:str) -> bool:
+
+# Two Pointer Solution
+def isPalindrome1(s:str) -> bool:
+    i,j = 0, len(s)-1
+    while i <= j:
+        if isAlphaNumeric(i) and isAlphaNumeric(j):
+            if lower(s[i]) != lower(s[j]):
+                return False
+            i += 1
+            j -= 1
+        else:
+            if not isAlphaNumeric(s[i]):
+                i += 1
+            if not isAlphaNumeric(s[j]):
+                j -= 1
+    return True
+    
+def isAlphaNumeric(s:str) -> bool:
+    asciiValueOfS = ord(s)
+    if (asciiValueOfS >= 48 and asciiValueOfS <= 57) or (asciiValueOfS >= 65 and asciiValueOfS <= 90) or (asciiValueOfS >= 97 and asciiValueOfS <= 122):
+        return True
+    return False
+
+def lower(s:str) -> str:
+    asciiValueOfS = ord(s)
+    if (asciiValueOfS >= 65 and asciiValueOfS <= 90):
+        return chr(asciiValueOfS+32)
+    else:
+        return s
+    
+# Reverse String Solution
+def isPalindrome2(s:str) -> bool:
     validS = ""
     for c in s:
         if c.isalnum():
             validS += c.lower()
-    i,j = 0, len(validS)
-    while i <= j:
-        if validS[i] != validS[j]:
-            return False
-        i += 1 
-        j -= 1
-    return True
+    return validS == validS[::-1]
