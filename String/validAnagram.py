fe3f2f108b4e59f4ecc35ca5984de39b59f6b7c2
Bhargavaa1 <abhargava@g.hmc.edu>
2022-07-11 02:48:19 -0400

    Valid Anagram 2 and 3

--- String/validAnagram.py
+++ String/validAnagram.py
@@ -1,7 +1,8 @@
+from collections import Counter
 #  242. Valid Anagram
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
-def isAnagram(s:str, t:str) -> bool:
+def isAnagram1(s:str, t:str) -> bool:
     sCharCount = {}
     for chr in s:
         if chr not in sCharCount.keys():
@@ -17,3 +18,9 @@ def isAnagram(s:str, t:str) -> bool:
         if sCharCount[chr] != 0:
             return False
     return True
+
+def isAnagram2(s: str, t: str) -> bool:
+    return sorted(s) == sorted(t)
+
+def isAnagram3(s: str, t: str) -> bool:
+    return Counter(s) == Counter(t)
