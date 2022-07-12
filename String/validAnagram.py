d37a684d2f4aea19e7b03b1f7e61a63cea74d97e
Bhargavaa1 <abhargava@g.hmc.edu>
2022-07-12 00:38:10 -0400

    Valid Anagram Annotations

--- String/validAnagram.py
+++ String/validAnagram.py
@@ -1,7 +1,11 @@
 from collections import Counter
 #  242. Valid Anagram
 # Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+# typically using all the original letters exactly once.
+
+# Runtime: O(n) Space: O(1)
+# Custom Character Counter 
 def isAnagram1(s:str, t:str) -> bool:
     sCharCount = {}
     for chr in s:
@@ -19,8 +23,12 @@ def isAnagram1(s:str, t:str) -> bool:
             return False
     return True
 
+# Runtime: O(n log n) Space: O(n)
+# Sorting
 def isAnagram2(s: str, t: str) -> bool:
     return sorted(s) == sorted(t)
 
+# Runtime: O(n) Space: O(1)
+# Python's Character Counter
 def isAnagram3(s: str, t: str) -> bool:
     return Counter(s) == Counter(t)
